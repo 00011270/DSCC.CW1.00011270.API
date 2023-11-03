871121bbcc12be037ce4dd3403ec2a6d9ff672ad
00011270 <wiut11270@gmail.com>
2023-11-03 22:06:58 +0500

    Configuring Connection string to RDS database

---
+++ BlogPlatform/DAL/BlogContext.cs
@@ -0,0 +1,19 @@
+﻿using BlogApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.DAL
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public BlogContext(DbContextOptions<BlogContext> o) : base(o) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
